7752397c7cd2ae26ffb64a9e982a43686d738f1e
vladimir <shagor4423@gmail.com>
2018-12-24 16:47:19 +0300

    Images Api

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = '7145fbb1b2671b2';
 //const CLIENT_SECRET = '969b57537f6073cb263e77e9bbe231f3bb75a43b';
@@ -14,4 +15,11 @@ export default {
     window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
         querystring)}`;
   },
+  fetchImages(token) {
+    return axios.get(`${ROOT_URL}/3/account/me/images`, {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+  }
 };
